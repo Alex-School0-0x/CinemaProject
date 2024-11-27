166683b8b0f3435fa3429329bcd1df661335b884
alex-school0-0x <alth2@elev.tec.dk>
2024-11-27 12:55:00 +0100

    ver. 1

--- CinemaProject/CinemaContext.cs
+++ CinemaProject/CinemaContext.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CinemaProject.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace CinemaProject
 {
     public class CinemaContext : DbContext
     {
-
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Genre> Genres { get; set; }
     }
 }
